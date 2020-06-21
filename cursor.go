1a13be52d3f026e62ad4f2e9e8505db8f485c562
王一飞 <codeyifei@163.com>
2020-06-21 13:10:47 +0800

    封装了光标上下移和清空整行内容的函数

---
+++ cursor.go
@@ -0,0 +1,29 @@
+package helper
+
+import "fmt"
+
+func CursorUp() {
+	fmt.Print("\033[1A")
+}
+
+func CursorUpLines(line int) {
+	fmt.Printf("\033[%dA", line)
+}
+
+func CursorDown() {
+	fmt.Println()
+}
+
+func CursorDownLines(line int) {
+	if line >= 0 {
+		for i := 0; i < line; i++ {
+			fmt.Println()
+		}
+	} else {
+		CursorUpLines(-line)
+	}
+}
+
+func EmptyLine() {
+	fmt.Print("\r\033[K")
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/codeyifei/go-fmt-helper
+
+go 1.14
